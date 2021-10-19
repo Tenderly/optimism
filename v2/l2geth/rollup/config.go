ebcb54543f2a6ac74d8b1af49361f821f6b6a745
Marko Bakovic <markobakovic95@gmail.com>
2021-10-19 15:06:46 +0200

    Copy l2geth to v2

---
+++ v2/l2geth/rollup/config.go
@@ -0,0 +1,44 @@
+package rollup
+
+import (
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type Config struct {
+	// Maximum calldata size for a Queue Origin Sequencer Tx
+	MaxCallDataSize int
+	// Verifier mode
+	IsVerifier bool
+	// Enable the sync service
+	Eth1SyncServiceEnable bool
+	// Gas Limit
+	GasLimit uint64
+	// HTTP endpoint of the data transport layer
+	RollupClientHttp string
+	// Owner of the GasPriceOracle contract
+	GasPriceOracleOwnerAddress common.Address
+	// Turns on checking of state for L2 gas price
+	EnableL2GasPolling bool
+	// Deployment Height of the canonical transaction chain
+	CanonicalTransactionChainDeployHeight *big.Int
+	// Polling interval for rollup client
+	PollInterval time.Duration
+	// Interval for updating the timestamp
+	TimestampRefreshThreshold time.Duration
+	// Represents the source of the transactions that is being synced
+	Backend Backend
+	// Only accept transactions with fees
+	EnforceFees bool
+	// Prevent transactions with a L2 gas limit lower than this value
+	// The L2 gas limit is parsed from the `tx.gasPrice`, see the
+	// `rollup/fees` package for more information
+	MinL2GasLimit *big.Int
+	// Allow fees within a buffer upwards or downwards
+	// to take fee volatility into account between being
+	// quoted and the transaction being executed
+	FeeThresholdDown *big.Float
+	FeeThresholdUp   *big.Float
+}
